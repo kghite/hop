e9234c1830108ba4f4c655d0b45317d046f5e010
kghite <kghite@gmail.com>
2020-08-02 08:31:21 -0400

    ADD: Sensor streams

--- packages/hop_onboard/src/hindbrain.cpp
+++ packages/hop_onboard/src/hindbrain.cpp
@@ -6,7 +6,7 @@
  * - Execute cmd_vel from controller
  * 
  * Subscribed: /cmd_vel, /safety_server
- * Published: /camera/image, /imu, /safety_onboard, /estop
+ * Published: /safety_onboard, /estop
  */
 
 #include <ros/ros.h>
@@ -19,10 +19,8 @@
 
 
 // Declaring publishers
-ros::Publisher image_pub;
-ros::Publisher imu_pub;
-ros::Publisher safety_pub;
 ros::Publisher estop_pub;
+ros::Publisher safety_pub;
 
 
 /*
@@ -30,17 +28,6 @@ ros::Publisher estop_pub;
  */
 void check_timeout() {
 
-}
-
-
-/*
- * Publish sensor streams back to the offboard controller
- */
-void manage_sensor_stream() {
-
-	// Read and package sensor streams from python i/o or ROS node
-
-	// Publish streams
 }
 
 
@@ -71,19 +58,16 @@ int main(int argc, char**argv) {
     ros::init(argc, argv, "hindbrain");
 	
     // ROS Publishers
-	image_pub = nh.advertise<geometry_msgs::Twist>("/camera/image", 1000);
-    imu_pub = nh.advertise<sensor_msgs::Imu>("/imu", 1000);
     safety_pub = nh.advertise<std_msgs::Int8>("/safety_onboard", 1000);
     estop_pub = nh.advertise<std_msgs::Int8>("/estop", 1000);
 
     // ROS Subscribers
-    ros::Subscriber cmd_sub = n.subscribe("/cmd_vel", 1000, cmd_callback);
-    ros::Subscriber safety_sub = n.subscribe("/safety_server", 1000, safety_callback);
+    ros::Subscriber cmd_sub = nh.subscribe("/cmd_vel", 1000, cmd_callback);
+    ros::Subscriber safety_sub = nh.subscribe("/safety_server", 1000, safety_callback);
 
 	ros::Rate r(10);
 	while(ros::ok()) {
         check_timeout();
-        manage_sensor_stream();
 
 		r.sleep();
         ros::spinOnce();
