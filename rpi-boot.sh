331c8a51774b73db41704e2ac51096ad38c4a1d5
kghite <kghite@gmail.com>
2020-08-09 21:40:32 +0100

    UPD: ROS linking

--- rpi-boot.sh
+++ rpi-boot.sh
@@ -4,5 +4,10 @@
 cd catkin_ws/src/hop && git pull origin rpi
 echo Updated Git Repo
 
+# Link ROS Master
+export ROS_MASTER_URI=http://kghite.local:11311
+export ROS_IP=mahri.local
+echo Set up ROS Master link to kghite.local
+
 # Start robot scripts
 echo Finished Mahri Startup
